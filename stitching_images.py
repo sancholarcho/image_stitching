5f7db144e396c6e6585564f8a02b3d774a1c6192
sancho_larcho <insane.smiley@gmail.com>
2021-03-14 17:39:48 +0300

    Progressbar and  save path choice added

--- stitching_images.py
+++ stitching_images.py
@@ -1,4 +1,5 @@
 import pyodbc
+import logging
 # import matplotlib.pyplot as plt
 import os
 from pathlib import Path
@@ -12,52 +13,69 @@ import yaml
 import threading
 
 
-
 class MainFrame(Frame):
     def __init__(self):
         super().__init__()
+        self.folders_number_total = 0
         self.stitching_running = False
+        self.progress_total = 0
 
         self.db_file_path_label = Label(self, text="Путь к файлу базы данных:")
-        self.db_file_path_label.grid(row=0, columnspan=3)
+        self.db_file_path_label.grid(row=0, columnspan=4)
         self.db_file_path = StringVar()
         self.db_file_path_field = Entry(self, textvariable=self.db_file_path, width=80)
         self.db_file_path_button = Button(self, command=self.browse_files, text='Выбрать')
-        self.db_file_path_field.grid(row=1, column=1, columnspan=2)
+        self.db_file_path_field.grid(row=1, column=1, columnspan=3, padx=5)
         self.db_file_path_button.grid(row=1, column=0)
 
         self.images_folder_path_label = Label(self, text="\nПуть к одной папке с изображениями:")
-        self.images_folder_path_label.grid(row=3, columnspan=3)
+        self.images_folder_path_label.grid(row=3, columnspan=4)
         self.images_folder_path = StringVar()
         self.images_folder_path_field = Entry(self, textvariable=self.images_folder_path, width=80)
-        self.images_folder_path_button = Button(self, command=lambda: self.browse_directory('one_folder'), text='Выбрать')
-        self.images_folder_path_field.grid(row=4, column=1, columnspan=2)
+        self.images_folder_path_button = Button(self, command=lambda: self.browse_directory(self.images_folder_path),
+                                                text='Выбрать')
+        self.images_folder_path_field.grid(row=4, column=1, columnspan=3, padx=5)
         self.images_folder_path_button.grid(row=4, column=0)
 
         self.dirs_folder_path_label = Label(self, text="\nПуть к нескольким папкам с изображениями:")
-        self.dirs_folder_path_label.grid(row=5, columnspan=3)
+        self.dirs_folder_path_label.grid(row=5, columnspan=4)
         self.dirs_folder_path = StringVar()
         self.dirs_folder_path_field = Entry(self, textvariable=self.dirs_folder_path, width=80)
-        self.dirs_folder_path_button = Button(self, command=lambda: self.browse_directory('multiple_folders'), text='Выбрать')
-        self.dirs_folder_path_field.grid(row=6, column=1, columnspan=2)
+        self.dirs_folder_path_button = Button(self, command=lambda: self.browse_directory(self.dirs_folder_path),
+                                              text='Выбрать')
+        self.dirs_folder_path_field.grid(row=6, column=1, columnspan=3, padx=5)
         self.dirs_folder_path_button.grid(row=6, column=0)
 
+        self.save_path_label = Label(self, text="\nПуть к папке сохранения:")
+        self.save_path_label.grid(row=7, columnspan=4)
+        self.save_path = StringVar()
+        self.save_path_field = Entry(self, textvariable=self.save_path, width=80)
+        self.save_path_button = Button(self, command=lambda: self.browse_directory(self.save_path),
+                                       text='Выбрать')
+        self.save_path_field.grid(row=8, column=1, columnspan=3, padx=5)
+        self.save_path_button.grid(row=8, column=0)
+
         self.program_id_label = Label(self, text="\nID программы:")
-        self.program_id_label.grid(row=7, column=0)
+        self.program_id_label.grid(row=9, column=0)
         self.program_id = IntVar()
         self.program_id_label_field = Entry(self, textvariable=self.program_id, width=6)
-        self.program_id_label_field.grid(row=7, column=1, sticky='sw')
+        self.program_id_label_field.grid(row=9, column=1, sticky='sw', padx=5)
+
+        self.height_label = Label(self, text="\nРасстояние от детектора до слоя изделия, мм:")
+        self.height_label.grid(row=9, column=2, sticky='se')
+        self.height = IntVar()
+        self.height_label_field = Entry(self, textvariable=self.height, width=6)
+        self.height_label_field.grid(row=9, column=3, sticky='sw', padx=5)
 
         self.run_stitching_button = Button(self, command=self.start_stitching_in_thread, text='Сшить изображения')
-        self.run_stitching_button.grid(row=7, column=2, sticky='sw')
+        self.run_stitching_button.grid(row=10, column=3)
 
         self.progress_bar_label = Label(self, text='\n')
-        self.progress_bar = Progressbar(self, orient=HORIZONTAL, length=100, mode='determinate')
+        self.progress_bar_label.grid(row=10)
+        self.progress_bar = Progressbar(self, orient=HORIZONTAL, length=400, mode='determinate')
+        self.progress_bar.grid(row=10, column=0, columnspan=3, sticky='e', pady=20, padx=5)
 
         self.read_config_file()
-
-        if Path.cwd().joinpath('result').exists() is False:
-            Path.cwd().joinpath('result').mkdir()
 
     def browse_files(self):
         filename = filedialog.askopenfilename(initialdir="/",
@@ -68,12 +86,9 @@ class MainFrame(Frame):
                                                           "*.*")))
         self.db_file_path.set(filename)
 
-    def browse_directory(self, what):
+    def browse_directory(self, variable):
         directory = filedialog.askdirectory()
-        if what == 'one_folder':
-            self.images_folder_path.set(directory)
-        if what == 'multiple_folders':
-            self.dirs_folder_path.set(directory)
+        variable.set(directory)
 
     def browse_folders(self, multiple_directory):
         folders_list = []
@@ -81,10 +96,12 @@ class MainFrame(Frame):
         for item in item_list:
             if Path.is_dir(Path(multiple_directory).joinpath(item)):
                 folders_list.append(Path(multiple_directory).joinpath(item))
+        self.folders_number = len(folders_list)
         return folders_list
 
     def image_stitching(self, **kwargs):
         """Initial things"""
+        logging.info('Start stitching')
         coord_catalog = []
         n_catalog = []
         x_catalog = []
@@ -113,6 +130,13 @@ class MainFrame(Frame):
         cur.close()
         conn.close()
 
+        '''
+        ^ Y
+        |
+        |
+        -------> X
+        '''
+
         '''Making lists with raw coordinates for chosen program'''
         program_id = self.program_id.get()
         for entry in coord_catalog:
@@ -128,17 +152,19 @@ class MainFrame(Frame):
         y_min = min(y_min)
 
         '''Enter scale factor'''
-        # TODO: Fixed scale values for X and Y axes, editable 'height' value!
-        # scale_factor = 142.86 * 3.025  # Top fit
-        scale_factor = 142.86 * 2.975  # Pipes fit  ******* This value can be edited *******
-        print(f'Scale factor: {scale_factor}')
+        height = int(self.height.get())
+        focal_length = 850  # 2432 # TODO: May be need to clarify this value
+        step_x = 3564.55  # Calculated with ruler, x_value/mm
+        # step_y = 6800
+        step_y = 6168.44  # Calculated with ruler, y_value/mm
+        scale_factor = focal_length / height
+        logging.info(f'Scale factor: {scale_factor:.2f}')
 
         '''Making normalized and scaled coordinates'''
-        norm_x_catalog = [(x - x_min) / scale_factor for x in x_catalog]
-        norm_y_catalog = [(y - y_min) / scale_factor / 2 for y in y_catalog]
+        norm_x_catalog = [((x - x_min) / step_x) * scale_factor for x in x_catalog]
+        norm_y_catalog = [((y - y_min) / step_y) * scale_factor for y in y_catalog]
 
         '''Making plot'''  # Currently turned off
-        # plt.scatter(x_catalog, y_catalog)  # Get raw coordinates to plot
         # plt.scatter(norm_x_catalog, norm_y_catalog)  # Get normalized and scaled coordinates to plot
         # plt.ylabel('Y')
         # plt.xlabel('X')
@@ -167,9 +193,18 @@ class MainFrame(Frame):
                        int(max(norm_y_catalog)) + 1024 - cropping_size + 500)  # 1024 - size of not cropped image
         pillow_image = pil_image.new('RGB', pillow_size)
 
-        def y_inversing(norm_y):  # Convert normalized coordinates to pillow coordinate system (Y vice versa)
-            inversed_y = max(norm_y_catalog) - norm_y
-            return inversed_y
+        def y_inverting(norm_y):  # Convert normalized coordinates to pillow coordinate system (Y vice versa)
+            inverted_y = max(norm_y_catalog) - norm_y
+            return inverted_y
+
+        '''
+        X and Y pillow axes schema
+
+        -------> X
+        |
+        |
+        v Y
+        '''
 
         '''**************'''
         '''Here supposed to make transparency for each image in size as pillow_image.
@@ -179,71 +214,101 @@ class MainFrame(Frame):
         '''Pasting images to draw'''
         number_of_columns = 3  # ******* This value can be edited *******
         #  'number_of_columns' # TODO: supposed to be a function's return value in future
+        image_shifting_px = -5  # If images have unfixed rotation ******* This value can be edited *******
         column_counter = 0
         row_counter = 1
-        row_shifting_px = 40  # ******* This value can be edited *******
+        row_shifting_px = 0  # TODO: check how this value influences with height change
         for image_num in images:
             column_counter += 1
             if column_counter > number_of_columns:
                 row_counter += 1
                 column_counter = 1
-            image_shifting_px = 5  # If images have unfixed rotation ******* This value can be edited *******
-            pillow_image.paste(images.get(image_num),
-                               (int(images.get(image_num).x_pos) - image_shifting_px * (row_counter - 1),
-                                int(y_inversing(images.get(image_num).y_pos)) +
-                                image_shifting_px * column_counter +
-                                row_counter * row_shifting_px))
-
+            logging.info(f'X={images.get(image_num).x_pos:.2f}, Y={y_inverting(images.get(image_num).y_pos):.2f}')
+            dx = image_shifting_px * (row_counter - 1)
+            dy = - image_shifting_px * (column_counter - 1) + row_counter * row_shifting_px
+            logging.info(f'dX={dx}, dY={dy}')
+            x_now = round(images.get(image_num).x_pos) + dx
+            y_now = round(y_inverting(images.get(image_num).y_pos)) + dy
+            logging.info(f'X={x_now}, Y={y_now}')
+            logging.info('-----------')
+            pillow_image.paste(images.get(image_num), (x_now, y_now))
         '''Saving image'''
         now_is = datetime.datetime.now().strftime('%Y_%m_%dT%H_%M_%S')
         last_folder_name = directory.parts[-1]
         image_file_type = '.png'  # ******* This value can be edited *******
-        new_dir_name = global_path.joinpath("result")
+        new_dir_name = Path(self.save_path.get()).joinpath('result')
         new_file_name = f'{last_folder_name}_result_{now_is}{image_file_type}'
+        if new_dir_name.exists() is False:
+            new_dir_name.mkdir()
         pillow_image.save(new_dir_name.joinpath(new_file_name))
+
+        '''Progressbar'''
+        self.progress = 100 / self.folders_number_total
+        self.progress_total += self.progress
+        self.progress_bar['value'] = self.progress_total
+        self.update_idletasks()
+
+        logging.info('Image stitched')
 
     def image_stitching_from_multiple_dirs(self):
         folders_list = self.browse_folders(self.dirs_folder_path.get())
+        self.folders_number_total += len(folders_list)
         for directory in folders_list:
             self.image_stitching(directory=directory)
 
     def image_stitching_from_one_dir(self):
         directory = self.images_folder_path.get()
+        self.folders_number_total += 1
         self.image_stitching(directory=directory)
 
     def read_config_file(self):
         try:
-            print('Reading config file')
+            logging.info('Reading config file')
             with open('config.yaml', 'r') as file:
                 data_dict = yaml.load(file, Loader=yaml.FullLoader)
 
-        except:
-            print('No config file found')
+        except FileNotFoundError:
+            logging.warning('No config file found')
         try:
             db_dir = data_dict.get('db_dir')
             single_dir = data_dict.get('single_dir')
             multiple_dir = data_dict.get('multiple_dir')
-            prog_num = data_dict.get('prog_num')
+            program_num = data_dict.get('program_num')
+            save_dir = data_dict.get('save_dir')
+            height = data_dict.get('height')
             self.db_file_path.set(db_dir)
             self.images_folder_path.set(single_dir)
             self.dirs_folder_path.set(multiple_dir)
-            self.program_id.set(prog_num)
-        except:
-            print('Cant load values')
+            self.program_id.set(program_num)
+            self.save_path.set(save_dir)
+            self.height.set(height)
+        except (UnboundLocalError, KeyError):
+            self.db_file_path.set(None)
+            self.images_folder_path.set(None)
+            self.dirs_folder_path.set(None)
+            self.program_id.set(None)
+            self.save_path.set(None)
+            self.height.set(None)
+            logging.info('Cant load values, setting None')
 
     def write_config_file(self):
         db_dir = self.db_file_path.get()
         single_dir = self.images_folder_path.get()
         multiple_dir = self.dirs_folder_path.get()
-        prog_num = self.program_id.get()
+        program_num = self.program_id.get()
+        save_dir = self.save_path.get()
+        height = self.height.get()
         data_dict = {'db_dir': db_dir,
                      'single_dir': single_dir,
                      'multiple_dir': multiple_dir,
-                     'prog_num': prog_num}
+                     'program_num': program_num,
+                     'save_dir': save_dir,
+                     'height': height}
         with open('config.yaml', 'w') as file:
             yaml.dump(data_dict, file)
 
     def start_stitching(self):
+        self.progress_total = 0
         if len(self.dirs_folder_path.get()) != 0:
             self.image_stitching_from_multiple_dirs()
         if len(self.images_folder_path.get()) != 0:
@@ -251,6 +316,7 @@ class MainFrame(Frame):
         self.write_config_file()
         self.stitching_running = False
         self.run_stitching_button.configure(state=NORMAL)
+        self.folders_number_total = 0
 
     def start_stitching_in_thread(self):
         self.stitching_running = True
@@ -258,9 +324,9 @@ class MainFrame(Frame):
         self.start_stitching_thread = threading.Thread(target=self.start_stitching)
         self.start_stitching_thread.start()
 
-    # TODO: Progress bar function
 
 class MainApplication(Tk):
+
     def __init__(self):
         super().__init__()
         self.title('Image Stitching')
@@ -269,8 +335,7 @@ class MainApplication(Tk):
         self.frame.grid()
 
 
+logging.basicConfig(level=logging.DEBUG)
 app = MainApplication()
-global_path = Path.cwd()
-# app.geometry('700x700')  # wide x height
 app.resizable(width=False, height=False)
 app.mainloop()
